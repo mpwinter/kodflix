d0d30c96ca2709813484b2778b15d1eb2b82f605
Mark <mp.winter@yahoo.co.uk>
2020-08-19 16:21:20 +0100

    Check up-to-date

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Technologies from './Technologies'
+import Details from './Details/Details';
+import NotFound from './NotFound';
+
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Welcome to Kodflix!!!</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+      <Route exact path='/' component={Technologies} />
+      <Route exact path='/not-found' component={NotFound} />
+      <Route path='/:technologyId' component={Details} />
+      </Switch>
     </div>
+    </Router>
   );
 }
+
+
 
 export default App;
